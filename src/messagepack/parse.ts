376689e7e5e8f21da2514bb92246482f3456bc87
David Meister <thedavidmeister@gmail.com>
2020-10-23 17:48:12 +0400

    lint

--- src/messagepack/parse.ts
+++
@@ -1,3 +0,0 @@
-export function parse(guard:any, build:any, value:any, name:string):any|Error {
- return guard(value) ? build(value) : Error('failed to parse a ' + name + ' from: ' + JSON.stringify(value))
-}
